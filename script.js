4d2cec5710a06784ff259992cf6f03334d4d3ef2
Alexis Nalls <alexis.n.nalls@gmail.com>
2017-01-30 23:45:06 +0000

    hexagon

--- script.js
+++ script.js
@@ -12,7 +12,34 @@ right(90);
 forward(50);
 }
 
-// Type your function call below
+function triangle(){
+right(60);
+forward(50);
+right(60);
+forward(50);
+right(150);
+forward(75);
+}
 
+function hexagon(){
+    forward(100);
+    left(40);
+    forward(40);
+    left(100);
+    forward(40);
+    left(40);
+    forward(50);
+    left(40);
+    forward(40);
+    left(100);
+    forward(40);
+}
+function house(){
+square();
+triangle();
+}
+// Type your function call below
+house();
+hexagon();
 
 stamp();
